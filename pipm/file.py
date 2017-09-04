0a53380888d45f609f745a705e86e1bb69da0622
jnoortheen <jnoortheen@gmail.com>
2017-09-04 14:43:45 +0530

    fix: recursive paths

--- pipm/file.py
+++ pipm/file.py
@@ -9,19 +9,22 @@ def append_last_line(filename):
     Args:
         filename:
     >>> f = get_requirements_filename('dev')
-    >>> with open(f) as fl: fl.read()
+    >>> with open(f) as fl: fl.read().strip()
     '-r requirements.txt'
     >>> os.remove(f)
     >>> os.remove('requirements.txt')
     """
     with open(filename, 'ab+') as f:
-        # check first byte. if it is empty then it may be an empty file
+        # check for empty file
+        f.seek(0)
+        if f.read(1) == b'' and f.read() == b'':
+            return
         f.seek(-2, os.SEEK_END)
         if f.read(1) != b'\n':
             f.write(b'\n')
 
 
-def get_env_requirement_file(*paths):
+def get_env_requirement_file(*paths, **kwargs):
     """
 
     Args:
@@ -50,10 +53,11 @@ def get_env_requirement_file(*paths):
                 raise
 
     if not os.path.exists(filename):
-        basename = get_requirements_filename()
         with open(filename, 'wb') as f:
-            if filename != basename:
-                f.write('-r {}'.format('base.txt' if 'base' in basename else basename).encode('utf-8'))
+            if kwargs.get('env') != '':
+                basename = get_requirements_filename()
+                if filename != basename:
+                    f.write('-r {}'.format('base.txt' if 'base' in basename else basename).encode('utf-8'))
 
     return filename
 
@@ -105,8 +109,8 @@ def get_requirements_filename(env=''):
     envs = ('dev', 'development',) if env == 'dev' else ('prod', 'production',) if env == 'prod' else (
         'test',) if env == 'test' else (env,) if env else ('base',)
     paths = get_patterns(*envs)
-    fname = get_env_requirement_file(*paths)
-    # append_last_line(fname)
+    fname = get_env_requirement_file(*paths, env=env)
+    append_last_line(fname)
     return fname
 
 
