16db7a94d1077ff239038a9923784c4682a171f9
jnoortheen <jnoortheen@gmail.com>
2017-09-08 12:43:20 +0530

    test: updates

--- tests/test_file_functions.py
+++ tests/test_file_functions.py
@@ -1,9 +1,12 @@
-import os
 import codecs
+import os
+
 from py._path.local import LocalPath
+import pytest
+
 from pipm import file
 
-reqs = """\
+REQS_STR = """\
 # standard package name alone
 requests
 # package with more options
@@ -14,14 +17,21 @@ FooProject >= 1.2 --global-option="--no-user-cfg" \
 -e git+https://github.com/d0c-s4vage/pyactlab.git@feather-master#egg=pyactlab
 -e git+git@git.myproject.org:MyProject#egg=MyProject
 # with hash
-FooProject == 1.2 --hash=sha256:2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824 \
+Foo2Project == 1.2 --hash=sha256:2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824 \
                   --hash=sha256:486ea46224d1bb4fb680f34f7c9ad96a8f24ec88be73ea8e5a6c65260e9cb8a7a
 """
-dev_reqs = """\
+DEV_REQS_STR = """\
 -r requirements.txt
 
 some_content==0.1.0 # with inline comment you need to write this back
 """
+
+
+@pytest.fixture
+def chdir(tmpdir_factory):
+    tmpdir = tmpdir_factory.mktemp('test')
+    os.chdir(tmpdir.strpath)
+    return tmpdir
 
 
 def test_append_last_line(tmpdir):
@@ -41,7 +51,7 @@ def test_get_env_reqfile_case1(tmpdir):
     assert f == paths[1]
 
 
-def test_get_env_reqfile_case2():
+def test_get_env_reqfile_case2(chdir):
     # case 2: creates file
     fname = 'some-requirements.txt'
     assert fname == file.get_env_reqfile(fname, base_file_name='requirements/base.txt', )
@@ -49,34 +59,60 @@ def test_get_env_reqfile_case2():
         cnt = f.read()
         assert cnt == '-r base.txt'
     assert os.path.exists(fname)
-    os.remove(fname)
 
 
-def test_get_req_filename(tmpdir):
+def test_get_req_filename(chdir):
     """
 
     Args:
         tmpdir (LocalPath): fixture
 
     """
-    os.chdir(tmpdir.strpath)
     assert file.get_req_filename('dev') == 'dev-requirements.txt'
     assert file.get_req_filename() == 'requirements.txt'
 
 
-def test_parse(tmpdir):
-    os.chdir(tmpdir.strpath)
+def test_parse_simple(chdir):
     fname = file.get_req_filename()
     with codecs.open(fname, 'w', 'utf-8') as f:
-        f.write(reqs)
+        f.write(REQS_STR)
 
     # single file parsing
     install_reqs = file.parse()
-    assert len(install_reqs) == 4
+    for r in install_reqs:
+        print(r)
+    assert len(install_reqs) == 5
 
+
+def test_parse_nested(chdir):
     # nested file parsing
+    with codecs.open(file.get_req_filename(), 'w', 'utf-8') as f:
+        f.write(REQS_STR)
     fname = file.get_req_filename('dev')
     with codecs.open(fname, 'w', 'utf-8') as f:
-        f.write(dev_reqs)
+        f.write(DEV_REQS_STR)
     install_reqs = file.parse('dev')
-    assert len(install_reqs) == 5
+    assert len(install_reqs) == 6
+
+
+def test_parse_comes_from(chdir):
+    forig = 'tests/fixtures/requirements.txt'
+    fname, line = file.parse_comes_from('-r {} (line 11)'.format(forig), 'dev')
+    assert fname == forig
+    assert line == 11
+
+    fname, line = file.parse_comes_from(None, 'dev')
+    assert fname == file.get_req_filename('dev')
+    assert line == None
+
+
+def test_uniq_reqs(chdir):
+    fname = file.get_req_filename()
+    with codecs.open(fname, 'w', 'utf-8') as f:
+        f.write(REQS_STR)
+    install_reqs = file.parse()
+    for r in install_reqs:
+        print(r.name)
+    ireqs = install_reqs + install_reqs
+    ireqs = file._uniq_resources(ireqs)
+    assert len(ireqs) == 5
