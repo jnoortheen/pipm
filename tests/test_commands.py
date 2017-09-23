627755694cd3472a1193ff6b9b48352eb1bf8e83
jnoortheen <jnoortheen@gmail.com>
2017-09-23 17:09:04 +0530

    test: reaching coverage of 94%

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,6 +1,10 @@
-from pipm import InstallCommandPlus, UpdateCommand
+from unittest import mock
+from unittest.mock import MagicMock, patch
+
+from pytest_mock import MockFixture
+
+from pipm import commands
 from pipm.file import get_req_filename
-from pip.req.req_install import InstallRequirement
 
 
 def test_fill_args_when_no_args_given(chdir):
@@ -11,7 +15,7 @@ def test_fill_args_when_no_args_given(chdir):
 
     """
     f = get_req_filename()
-    cmd = InstallCommandPlus()
+    cmd = commands.InstallCommandPlus()
     opts, args = cmd.parse_args([])
     assert opts.requirements == [f, ]
 
@@ -25,7 +29,7 @@ def test_fill_args_when_no_args_given_dev(chdir):
     """
     for env in ['dev', 'test', 'prod']:
         f = get_req_filename(env)
-        cmd = InstallCommandPlus()
+        cmd = commands.InstallCommandPlus()
         opts, args = cmd.parse_args(['--{}'.format(env)])
         assert opts.requirements == [f, ]
 
@@ -39,13 +43,13 @@ def test_fill_args_when_no_args_given_environ(chdir):
     """
     env = 'staging'
     f = get_req_filename(env)
-    cmd = InstallCommandPlus()
+    cmd = commands.InstallCommandPlus()
     opts, args = cmd.parse_args(['--env', env])
     assert opts.requirements == [f, ]
 
 
 def test_req_environment():
-    cmd = InstallCommandPlus()
+    cmd = commands.InstallCommandPlus()
     opts, args = cmd.parse_args(['--dev'])
     assert opts.req_environment == 'dev'
     opts, args = cmd.parse_args(['--test'])
@@ -59,6 +63,35 @@ def test_req_environment():
 
 
 def test_update_command_parse_args():
-    cmd = UpdateCommand()
+    cmd = commands.UpdateCommand()
     opts, args = cmd.parse_args([])
     assert opts.upgrade
+
+
+def test_install_cmd_run(mocker):
+    """
+    """
+    run = mocker.patch.object(commands.InstallCommand, 'run')  # type: MagicMock
+    save = mocker.patch.object(commands.file, 'save')  # type: MagicMock
+
+    # run method
+    cmd = commands.InstallCommandPlus()
+    opts, args = cmd.parse_args(['dev'])
+    cmd.run(opts, args)
+
+    run.assert_called_once()
+    save.assert_called_once()
+
+
+def test_remove_cmd_run(mocker):
+    run = mocker.patch.object(commands.UninstallCommand, 'run')  # type: MagicMock
+    save = mocker.patch.object(commands.file, 'save')  # type: MagicMock
+    mocker.patch.object(commands, 'get_orphaned_packages', return_value=['pkg1', 'pkg2'])  # type: MagicMock
+
+    # run method
+    cmd = commands.UninstallCommandPlus()
+    opts, args = cmd.parse_args(['dev'])
+    cmd.run(opts, args)
+
+    run.assert_called_once_with(opts, ['dev', 'pkg1', 'pkg2'])
+    save.assert_called_once()
