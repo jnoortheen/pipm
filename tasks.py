22fe7f42f3eb3d7ab59b78e552e6c68a46e16982
jnoortheen <jnoortheen@gmail.com>
2017-09-23 13:36:23 +0530

    feat: using invoke script instead of fabric

---
+++ tasks.py
@@ -0,0 +1,32 @@
+from invoke import task, Context
+
+import pipm
+
+
+@task
+def release(ctx):
+    """
+        create new tag and push to git and PyPI
+    Args:
+        ctx (Context):
+    """
+    ctx.run('git push')
+    ctx.run('git tag {}'.format(pipm.__version__))
+    ctx.run('git push --tags')
+
+    # dont forget to have this file
+    # ~/.pypirc
+    # [distutils]
+    # index-servers =
+    #  pypi
+
+    # [pypi]
+    # repository: https://upload.pypi.org/legacy/
+    # username: jnoortheen
+    # password: pwd
+    ctx.run('python setup.py sdist upload')
+
+
+@task
+def test(ctx):
+    ctx.run('pytest --cov=pipm tests/')
