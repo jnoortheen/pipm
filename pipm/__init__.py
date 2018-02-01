de0794bebefdb2fc786647aa72e30295c186b03c
jnoortheen <jnoortheen@gmail.com>
2018-02-02 02:36:18 +0530

    refactor: adding alias u for update command

--- pipm/__init__.py
+++ pipm/__init__.py
@@ -19,14 +19,19 @@ def get_prog():
 pip.get_prog = get_prog
 pip.utils.get_prog = get_prog
 pip.basecommand.get_prog = get_prog
+
 pip.commands_dict[InstallCommandPlus.name] = InstallCommandPlus
+pip.commands_dict['i'] = InstallCommandPlus
+
 pip.commands_dict[UninstallCommandPlus.name] = UninstallCommandPlus
+pip.commands_dict['rm'] = UninstallCommandPlus
+
 pip.commands_dict[FreezeCommandPlus.name] = FreezeCommandPlus
 pip.commands_dict['save'] = FreezeCommandPlus
-pip.commands_dict['i'] = InstallCommandPlus
-pip.commands_dict['rm'] = UninstallCommandPlus
+
 pip.commands_dict['update'] = UpdateCommand
 pip.commands_dict['upgrade'] = UpdateCommand
+pip.commands_dict['u'] = UpdateCommand
 
 
 # endpatch
