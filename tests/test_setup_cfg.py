1cf3a836973ea95e33deadc89a8b4ae778b34f96
jnoortheen <jnoortheen@gmail.com>
2018-10-01 12:28:41 +0530

    test: add more tests for setup.cfg

--- tests/test_setup_cfg.py
+++ tests/test_setup_cfg.py
@@ -14,8 +14,12 @@ def test_add_requirements_dev_with_existing_config(config, requirement_set_facto
 
 
 def test_add_requirements_no_config_file(chdir, requirement_set_factory):
-    config = setup_cfg.add_requirements(user_reqs=requirement_set_factory("py==1.0.0"), env="dev")
-    assert config['options']['install_requires'] == "\nsix~=1.11.0"
+    config = setup_cfg.add_requirements(user_reqs=requirement_set_factory("six~=1.1.0", "py==1.0.0", ))
+    assert config['options']['install_requires'] == "\nsix~=1.1.0\npy==1.0.0"
+
+
+def test_add_dev_requirements_no_config_file(chdir, requirement_set_factory):
+    config = setup_cfg.add_requirements(user_reqs=requirement_set_factory("pytest~=3.7.2", "py==1.0.0"), env="dev")
     assert config['options.extras_require']['dev'] == "\npytest~=3.7.2\npy==1.0.0"
 
 
