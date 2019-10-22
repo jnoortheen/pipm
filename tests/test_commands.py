42ef598abe5b282c13b654d6f366a4b0a90b8466
Noortheen Raja <jnoortheen@gmail.com>
2019-10-22 23:18:28 +0530

    test: update test_commands.py to pass cmd and desc

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -12,9 +12,9 @@ def test_fill_args_when_no_args_given(chdir):
 
     """
     f = get_req_filename()
-    cmd = commands.InstallCommandPlus()
+    cmd = commands.InstallCommandPlus("i", "i")
     opts, args = cmd.parse_args([])
-    assert opts.requirements == [f, ]
+    assert opts.requirements == [f]
 
 
 def test_fill_args_when_no_args_given_dev(chdir):
@@ -24,11 +24,11 @@ def test_fill_args_when_no_args_given_dev(chdir):
         tmpdir (LocalPath): fixture
 
     """
-    for env in ['dev', 'test', 'prod']:
+    for env in ["dev", "test", "prod"]:
         f = get_req_filename(env)
-        cmd = commands.InstallCommandPlus()
-        opts, args = cmd.parse_args(['--{}'.format(env)])
-        assert opts.requirements == [f, ]
+        cmd = commands.InstallCommandPlus("i", "i")
+        opts, args = cmd.parse_args(["--{}".format(env)])
+        assert opts.requirements == [f]
 
 
 def test_fill_args_when_no_args_given_all(chdir):
@@ -39,13 +39,13 @@ def test_fill_args_when_no_args_given_all(chdir):
 
     """
     # create the below two files
-    get_req_filename('dev')
-    get_req_filename('test')
+    get_req_filename("dev")
+    get_req_filename("test")
 
     # start test
     fset = get_req_filenames()
-    cmd = commands.InstallCommandPlus()
-    opts, args = cmd.parse_args(['--all'])
+    cmd = commands.InstallCommandPlus("i", "i")
+    opts, args = cmd.parse_args(["--all"])
     assert set(opts.requirements) == fset
 
 
@@ -56,29 +56,29 @@ def test_fill_args_when_no_args_given_environ(chdir):
         tmpdir (LocalPath): fixture
 
     """
-    env = 'staging'
+    env = "staging"
     f = get_req_filename(env)
-    cmd = commands.InstallCommandPlus()
-    opts, args = cmd.parse_args(['--env', env])
-    assert opts.requirements == [f, ]
+    cmd = commands.InstallCommandPlus("i", "i")
+    opts, args = cmd.parse_args(["--env", env])
+    assert opts.requirements == [f]
 
 
 def test_req_environment():
-    cmd = commands.InstallCommandPlus()
-    opts, args = cmd.parse_args(['--dev'])
-    assert opts.req_environment == 'dev'
-    opts, args = cmd.parse_args(['--test'])
-    assert opts.req_environment == 'test'
-    opts, args = cmd.parse_args(['pkg_name', '--prod'])
-    assert opts.req_environment == 'prod'
-    opts, args = cmd.parse_args(['pkg_name', '--env', 'staging'])
-    assert opts.req_environment == 'staging'
+    cmd = commands.InstallCommandPlus("i", "i")
+    opts, args = cmd.parse_args(["--dev"])
+    assert opts.req_environment == "dev"
+    opts, args = cmd.parse_args(["--test"])
+    assert opts.req_environment == "test"
+    opts, args = cmd.parse_args(["pkg_name", "--prod"])
+    assert opts.req_environment == "prod"
+    opts, args = cmd.parse_args(["pkg_name", "--env", "staging"])
+    assert opts.req_environment == "staging"
     opts, args = cmd.parse_args([])
     assert opts.req_environment is None
 
 
 def test_update_command_parse_args():
-    cmd = commands.UpdateCommand()
+    cmd = commands.UpdateCommand("u", "u")
     opts, args = cmd.parse_args([])
     assert opts.upgrade
 
@@ -86,12 +86,12 @@ def test_update_command_parse_args():
 def test_install_cmd_run(mocker):
     """
     """
-    run = mocker.patch.object(commands.install.InstallCommand, 'run')  # type: MagicMock
-    save = mocker.patch.object(commands.file, 'save')  # type: MagicMock
+    run = mocker.patch.object(commands.install.InstallCommand, "run")  # type: MagicMock
+    save = mocker.patch.object(commands.file, "save")  # type: MagicMock
 
     # run method
-    cmd = commands.InstallCommandPlus()
-    opts, args = cmd.parse_args(['dev'])
+    cmd = commands.InstallCommandPlus("i", "i")
+    opts, args = cmd.parse_args(["dev"])
     cmd.run(opts, args)
 
     run.assert_called_once()
@@ -99,26 +99,30 @@ def test_install_cmd_run(mocker):
 
 
 def test_remove_cmd_run(mocker):
-    run = mocker.patch.object(commands.UninstallCommand, 'run')  # type: MagicMock
-    save = mocker.patch.object(commands.file, 'save')  # type: MagicMock
-    mocker.patch.object(commands, 'get_orphaned_packages', return_value=['pkg1', 'pkg2'])  # type: MagicMock
+    run = mocker.patch.object(commands.UninstallCommand, "run")  # type: MagicMock
+    save = mocker.patch.object(commands.file, "save")  # type: MagicMock
+    mocker.patch.object(
+        commands, "get_orphaned_packages", return_value=["pkg1", "pkg2"]
+    )  # type: MagicMock
 
     # run method
-    cmd = commands.UninstallCommandPlus()
-    opts, args = cmd.parse_args(['dev'])
+    cmd = commands.UninstallCommandPlus("u", "u")
+    opts, args = cmd.parse_args(["dev"])
     cmd.run(opts, args)
 
-    run.assert_called_once_with(opts, ['dev', 'pkg1', 'pkg2'])
+    run.assert_called_once_with(opts, ["dev", "pkg1", "pkg2"])
     save.assert_called_once()
 
 
 def test_freeze_cmd_run(mocker):
-    run = mocker.patch.object(commands.FreezeCommand, 'run')  # type: MagicMock
-    save = mocker.patch.object(commands.file, 'save')  # type: MagicMock
-    mocker.patch.object(commands, 'get_orphaned_packages', return_value=['pkg1', 'pkg2'])  # type: MagicMock
+    run = mocker.patch.object(commands.FreezeCommand, "run")  # type: MagicMock
+    save = mocker.patch.object(commands.file, "save")  # type: MagicMock
+    mocker.patch.object(
+        commands, "get_orphaned_packages", return_value=["pkg1", "pkg2"]
+    )  # type: MagicMock
 
     # run method
-    cmd = commands.FreezeCommandPlus()
+    cmd = commands.FreezeCommandPlus("f", "f")
     opts, args = cmd.parse_args([])
     cmd.run(opts, args)
 
