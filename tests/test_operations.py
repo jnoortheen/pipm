7355f68ace41289cf0e15d2670770ec17b5b38da
Noortheen Raja <jnoortheen@gmail.com>
2022-02-24 14:18:11 +0530

    fix: test coverage

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -2,22 +2,6 @@ from pip._internal.operations.freeze import FrozenRequirement
 from pipm.src import operations
 
 
-def test_get_distributions(mocker):
-    class Obj(object):
-        pass
-
-    dists = []
-    for i in range(10):
-        obj = Obj()
-        obj.project_name = "pkg_" + str(i)
-        dists.append(obj)
-
-    mocker.patch.object(operations, "get_installed_distributions", return_value=dists)
-    assert set(operations.get_distributions().keys()) == {
-        "pkg_" + str(i) for i in range(10)
-    }
-
-
 def test_get_frozen_reqs(patched_dists):
     freqs = operations.get_frozen_reqs()
     assert len(freqs) == patched_dists.cnt
